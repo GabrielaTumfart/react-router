97c48987ec6749011104a3e520477e2c451c5e4d
GabrielaTumfart <gabrielatumfart@gmail.com>
2026-02-18 17:38:52 +0100

    add:initial routing structure with layout and pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,12 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import Products from "./pages/ProductsPage";
+import DefaultLayout from "./layouts/DefaultLayout";
+
 export default function App() {
  
 
@@ -10,6 +16,15 @@ export default function App() {
     <>
       
       <h1>React Router Store</h1>
+
+      <BrowserRouter>
+      <Routes>
+        <Route Component={DefaultLayout}>
+          <Route index Component={HomePage} />
+          <Route path="/chi-siamo" Component={AboutPage} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
     
        
     </>
