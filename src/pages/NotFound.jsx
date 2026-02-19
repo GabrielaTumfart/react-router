1c36dea20ef671309bf9e469d23b5423e5f196a7
GabrielaTumfart <gabrielatumfart@gmail.com>
2026-02-19 17:22:29 +0100

    add: implementata pagina 404 NotFound con messaggio di errore

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+export default function NotFound() {
+    return (
+      <div>
+        <h1>ERRORE 404 - Pagina non trovata</h1>
+        
+        <p>Ops, la pagina che cerchi non esiste.</p>
+
+        
+      </div>
+    );
+  }
+  
